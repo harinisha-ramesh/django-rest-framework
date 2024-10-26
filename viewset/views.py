db27db1200caa48482605bf7176170e4076c3297
harinisha-ramesh <harinisharamesh355@gmail.com>
2024-10-26 14:23:19 +0530

    update all

--- viewset/views.py
+++ viewset/views.py
@@ -80,35 +80,37 @@ class ProductGenericViewSet(mixins.ListModelMixin,
         # return response
 
 # from rest_framework import viewsets
-# from rest_framework.response import Response                                                                                                                                                                                                                                                                                                                     
+# from rest_framework.response import Response  
+
 
 # class ProductViewSet(viewsets.ViewSet):
+
     # def list(self, request):
-    #     products = Product.objects.all()
-    #     serializer = ProductSerializer(products, many=True)
-    #     return Response(serializer.data)
+        # products = Product.objects.all()
+        # serializer = ProductSerializer(products, many=True)
+        # return Response(serializer.data)
 
     # def create(self, request):
-    #     serializer = ProductSerializer(data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data )
-    #     return Response(serializer.errors)
+        # serializer = ProductSerializer(data=request.data)
+        # if serializer.is_valid():
+        #     serializer.save()
+        #     return Response(serializer.data )
+        # return Response(serializer.errors)
 
     # def retrieve(self, request, pk=None):
-    #     product = Product.objects.get(pk=pk)
-    #     serializer = ProductSerializer(product)
-    #     return Response(serializer.data)
+        # product = Product.objects.get(pk=pk)
+        # serializer = ProductSerializer(product)
+        # return Response(serializer.data)
 
     # def update(self, request, pk=None):
-    #     product = Product.objects.get(pk=pk)
-    #     serializer = ProductSerializer(product, data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data)
-    #     return Response(serializer.errors)
+        # product = Product.objects.get(pk=pk)
+        # serializer = ProductSerializer(product, data=request.data)
+        # if serializer.is_valid():
+        #     serializer.save()
+        #     return Response(serializer.data)
+        # return Response(serializer.errors)
 
     # def destroy(self, request, pk=None):
-    #     product = Product.objects.get(pk=pk)
-    #     product.delete()
+        # product = Product.objects.get(pk=pk)
+        # product.delete()
        
