2d195f5fcedbc6c592112ff308ab349cae1f6f72
harinisha-ramesh <harinisharamesh355@gmail.com>
2024-10-28 17:35:42 +0530

    update all

--- viewset/models.py
+++ viewset/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class product(models.Model):
     name: str = models.CharField(max_length=250, null = True)
     price: float = models.DecimalField(max_digits=10, decimal_places=3, null = True)
-    rating: float = models.FloatField(default=0)
+    rating: float= models.FloatField(default=0)
     is_deletable: bool = models.BooleanField(default=True)
 
     def __str__(self):
